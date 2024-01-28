0c24bcd095961ce6f81151ff541cc2583b353bf7
tsaikia <tsaikia@fanatics.com>
2024-01-28 20:36:33 +0530

    upgrade to aws v3

--- routes/common-routes.js
+++ routes/common-routes.js
@@ -1,53 +1,47 @@
 import { Router } from 'express'
-import AWS from 'aws-sdk'
+import { DynamoDBClient } from '@aws-sdk/client-dynamodb'
+import { PutCommand, DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb"
 import { randomUUID } from 'crypto'
 
 const router = Router()
 
-
-AWS.config.update({ region: process.env.AWS_REGION });
-
-const DynamoDB = new AWS.DynamoDB({
+const client = new DynamoDBClient({
+    region: process.env.AWS_REGION,
     credentials: {
       accessKeyId: process.env.AWS_ACCESS_KEY_ID,
       secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
     },
   })
 
+const docClient = DynamoDBDocumentClient.from(client);
+
 router.post('/add-post', async (req, res) => {
     if (!req.cookies.token)
         return res.status(401).json({ error: "Unauthorized" })
 
-    const { postBody, timestamp, postFrom } = req.body
+    const { postBody, timestamp, postFrom, timeToPost } = req.body
     const token = req.cookies.token
     const params = {
         TableName: process.env.TABLE_NAME,
         Item: {
-            postId: {
-                S: randomUUID()
-            },
-            token: {
-                S:  token
-            },
-            timestamp: {
-                S:  new Date(timestamp).getTime().toString()
-            },
-            postBody: {
-                S: postBody
-            },
-            postFrom: {
-                S: postFrom
-            }
+            postId: randomUUID(),
+            token,
+            timestamp,
+            postBody,
+            postFrom,
+            timeToPost
         }
     }
 
     try {
-        const data = await DynamoDB.putItem(params).promise();
-        console.log("Post Saved Successfully");
-        res.json({ id: data.id });
-      } catch (error) {
-        return res.status(500).json({ error });
-      }
+        const command = new PutCommand(params)
+        const response = await docClient.send(command)
+        console.log("Post Saved Successfully")
+        res.json(response)
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({ error })
+    }
 })
 
 export default router
