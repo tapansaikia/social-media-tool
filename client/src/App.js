46d67c4d597c7b0bffd7d6934d10052728d86cbf
tsaikia <tsaikia@fanatics.com>
2024-02-04 13:24:16 +0530

    add client

---
+++ client/src/App.js
@@ -0,0 +1,7 @@
+const App = () => {
+  return (
+    <>hello</>
+  )
+}
+
+export default App;
--- index.js
+++ index.js
@@ -1,19 +1,30 @@
 import express from "express"
 import dotenv from "dotenv"
 import bodyParser from "body-parser"
-import { readdir } from 'fs/promises';
+import { readdir } from 'fs/promises'
+import path from 'path'
+import { fileURLToPath } from 'url'
 
 dotenv.config()
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000
 
-const app = express();
+const app = express()
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+const publicDirectoryPath = path.join(__dirname, 'client/build')
 
 // middlewares
 app.use(bodyParser.urlencoded({ extended: false })) // parse application/x-www-form-urlencoded
 app.use(bodyParser.json()) // parse application/json
+app.use(express.static(publicDirectoryPath))
 
 // routes
+app.use('/', (req, res) => {
+  res.sendFile(path.join(publicDirectoryPath, 'index.html'))
+})
+
 const setupRoutes = async () => {
     try {
       const routeFiles = await readdir('./routes')
